fbe0c9c8b4125db13455ecb6534e13d72a0383a1
JohannesAldal-Jobloop <144998511+JohannesAldal-Jobloop@users.noreply.github.com>
2026-05-26 14:21:11 +0200

    Test på profil med embed

--- DiscordBot/main.py
+++ DiscordBot/main.py
@@ -38,4 +38,22 @@ with open("secrets.json") as f:
 async def sayHello(interaction: discord.Interaction):
     await interaction.response.send_message("Hi there.")
 
+@client.tree.command(name="din_profil", description="Din oppmøte og framgang på Digitale Talenter", guild=GUILD_ID)
+async def visEmbed(interaction: discord.Interaction, privat: bool):
+    embed = discord.Embed(
+        title=f"{interaction.user.display_name} sin profil på Digitale Talenter", 
+        #url="https://www.youtube.com/watch?v=KHQ2MaDbx5I&list=PL-7Dfw57ZZVQ-GCNQS4Kyz637Fffhb0Hs&index=7", 
+        description="Din framgang på DT",
+        color=discord.Color.purple()
+        )
+    embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
+    embed.set_thumbnail(url="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.webfx.com%2Fwp-content%2Fuploads%2F2021%2F10%2Fgeneric-image-placeholder.png&f=1&nofb=1&ipt=7a9a556c9a9fb1a83d6d420408365ee88b4c359380329e1db9363386b4287c57")
+    embed.add_field(name="Oppmøte", value="⬛⬛⬛⬛⬛⬛⬛⬛⬜⬜", inline=False)
+    embed.add_field(name="Sosial", value="⬛⬛⬛⬜⬜⬜⬜⬜⬜⬜")
+    embed.add_field(name="Inlevering", value="⬛⬛⬛⬛⬛⬛⬜⬜⬜⬜")
+    embed.set_image(url="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.webfx.com%2Fwp-content%2Fuploads%2F2021%2F10%2Fgeneric-image-placeholder.png&f=1&nofb=1&ipt=7a9a556c9a9fb1a83d6d420408365ee88b4c359380329e1db9363386b4287c57")
+    embed.set_footer(text=f"footer")
+
+    await interaction.response.send_message(embed=embed, ephemeral=privat)
+
 client.run(secrets["dicordbottoken"])
